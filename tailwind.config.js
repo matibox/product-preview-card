730cbf486313d0bc8c694ce1c53a17663b1c3536
Matibox <mateusz.hladky@gmail.com>
2022-12-22 15:18:55 +0100

    Finish card

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
   content: ['./src/index.html'],
   theme: {
     colors: {
-      cyan: 'hsl(158, 36%, 37%)',
+      cyan: {
+        normal: 'hsl(158, 36%, 37%)',
+        dark: 'hsl(158, 36%, 27%)',
+      },
       cream: 'hsl(30, 38%, 92%)',
       blue: {
         dark: 'hsl(212, 21%, 14%)',
         'dark-gray': 'hsl(228, 12%, 48%)',
       },
+      white: 'hsl(0, 0%, 100%)',
     },
     fontFamily: {
       sans: 'Montserrat, sans-serif',
